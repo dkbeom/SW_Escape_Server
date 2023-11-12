4b6c0199d534110d1eecb45b990078de65deaea8
dkbeom <namebdk@gmail.com>
2023-11-12 19:55:42 +0900

    초기 세팅

---
+++ swescape/src/main/java/com/example/swescape/SwescapeApplication.java
@@ -0,0 +1,13 @@
+package com.example.swescape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwescapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwescapeApplication.class, args);
+	}
+
+}
---
+++ swescape/src/test/java/com/example/swescape/SwescapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwescapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
